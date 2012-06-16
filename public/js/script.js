505744d4b2aec4d53eea68681a251856b3b4385f
Galex Yen <galex@reasoning.net>
2012-06-16 09:53:52 -0700

    Added modal dialog and calls to POST new items

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,11 @@ Application = Em.Application.create({
   
   ready: function() {
     Application.accountsController.fill();
+    
+    $('#addItemModal').modal();
+    $('#addItemModal').on('hidden', function() {
+      Application.addItemModal.hide();
+    });
   }
 });
 
@@ -34,21 +39,53 @@ Application.accountsController = Em.ArrayProxy.create({
     
     $.getJSON('/lists/passwords/all.json', function(data) {
       $(data).each(function(index, value) {
-        var account = me.createAccount(value);
+        var account = me.addItem(value);
       });
     });
   },
   
-  createAccount: function(account) {
+  addItem: function(account) {
     var newAccount = Application.Account.create(account);
     this.pushObject(newAccount);
   },
 
+  postNewItem: function(account) {
+    var me = this;
+    
+    $.ajax({
+      url: '/lists/passwords', 
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(account),
+      success: function(result, status, xhr) {
+        var path = xhr.getResponseHeader('Location');
+        var id = path.split('/').pop();
+        var newAccount = Application.Account.create(account);
+        newAccount.set('_id', id);
+        me.pushObject(newAccount);
+        
+        Application.addItemView.hide();
+      }
+    });
+  },
 });
 
 /*************************
  * Views
  *************************/
+Application.ListView = Em.View.extend({
+  listNameBinding: 'Application.listName',
+  
+  refresh: function(event) {
+    alert('refresh');
+  },
+  
+  addItem: function(event) {
+    $('#addItemModel input:first').focus();
+    $('#addItemModal').modal('show');
+  },
+}); 
+ 
 Application.ItemView = Em.View.extend({
   nameBinding: 'content.name',
   urlBinding: 'content.url',
@@ -56,3 +93,36 @@ Application.ItemView = Em.View.extend({
   passwordBinding: 'content.password',
 });
 
+Application.addItemView = Em.View.extend({
+  account: Application.Account.create(),
+  
+  hide: function() {
+    this.clearInputs();
+    $('#addItemModal').modal('hide');
+  },
+  
+  change: function(event) {
+    var account = this.get('account');
+    
+    if ('' != account.get('name') || '' != account.get('url') || '' != account.get('username') || '' != account.get('password'))
+      $('#addItemModal .submit').removeClass('disabled');
+    else
+      $('#addItemModal .submit').addClass('disabled');
+  },
+  
+  cancel: function() {
+    this.hide();
+  },
+  
+  submit: function() {
+    Application.accountsController.postNewItem(this.get('account'));
+  },
+  
+  clearInputs: function() {
+    var account = this.get('account');
+    account.set('name', '');
+    account.set('url', '');
+    account.set('username', '');
+    account.set('password', '');
+  }
+});
