86802e77ff3ddcd53628927be0f295a2ca74b6b4
Galex Yen <galex@reasoning.net>
2012-07-15 22:50:29 -0700

    Pulled out metadata json from admin.js script and generated it as part of admin.'s view.

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,283 +3,248 @@
 */
 
 $(function() {
-  /**************************
-   * Models
-   **************************/
-
-  var Field = Backbone.Model.extend({
-    defaults: function() {
-      return {
-        name: '',
-        heading: '',
-        placeholder: '',
-        type: 'String',
-        required: false,
-        default: '',
-      }
-    }
-  })
+  window.Admin = {};
   
-  var FieldCollection = Backbone.Collection.extend({
-    model: Field,
-  })
-
-  var Settings = Backbone.Model.extend({
-    idAttribute: 'name',
-    
-    urlRoot: '/api/metadata/lists',
-    
-    initialize: function(attributes) {
-      console.log('in initialize')
-      
-      if (attributes.hasOwnProperty('fields')) {
-        var fields = attributes.fields;
-        
-        var fieldCollection = new FieldCollection(fields);
-        
-        this.set('fields', fieldCollection);
-      }
-    },
-  })
+  (function(exports) {
   
-  var NewFieldView = Backbone.View.extend({
-    el: $('#newFieldModal'),
+    /**************************
+     * Models
+     **************************/
+    var Field = Backbone.Model.extend({
+      defaults: function() {
+        return {
+          name: '',
+          heading: '',
+          placeholder: '',
+          type: 'String',
+          required: false,
+          default: '',
+        }
+      }
+    })
+  
+    var FieldCollection = Backbone.Collection.extend({
+      model: Field,
+    })
 
-    events: {
-      'click #addFieldCancel'  : 'cancel',
-      'click #addFieldSubmit'  : 'submit',
-      'submit #newFieldForm'   : 'submit',
-      'shown'                  : 'shown',
-      'hidden'                 : 'onHidden',
-    },
+    var Settings = Backbone.Model.extend({
+      idAttribute: 'name',
+
+      urlRoot: '/api/metadata/lists',
     
-    initialize: function() {
-      this._modelBinder = new Backbone.ModelBinder();
+      initialize: function(attributes) {
+        if (attributes.hasOwnProperty('fields')) {
+          var fields = attributes.fields;
+        
+          var fieldCollection = new FieldCollection(fields);
+        
+          this.set('fields', fieldCollection);
+        }
+      },
+    })
+  
+    /**************************
+     * Views
+     **************************/
+    var NewFieldView = Backbone.View.extend({
+      el: $('#newFieldModal'),
+
+      events: {
+        'click #addFieldCancel'  : 'cancel',
+        'click #addFieldSubmit'  : 'submit',
+        'submit #newFieldForm'   : 'submit',
+        'shown'                  : 'shown',
+        'hidden'                 : 'onHidden',
+      },
+    
+      initialize: function() {
+        this._modelBinder = new Backbone.ModelBinder();
             
-      this.render();
-    },
+        this.render();
+      },
     
-    render: function() {
-      this._modelBinder.bind(this.model, this.el);
-    },
+      render: function() {
+        this._modelBinder.bind(this.model, this.el);
+      },
     
-    show: function() {
-      this.$el.modal('show');
-    },
+      show: function() {
+        this.$el.modal('show');
+      },
     
-    shown: function() {
-      this.$('input').first().focus();
-    },
+      shown: function() {
+        this.$('input').first().focus();
+      },
     
-    onHidden: function() {
-      this.model.set(this.model.defaults());
-    },
+      onHidden: function() {
+        this.model.set(this.model.defaults());
+      },
     
-    cancel: function(e) {
-      this.$el.modal('hide');
-    },
+      cancel: function(e) {
+        this.$el.modal('hide');
+      },
     
-    submit: function(e) {
-      e.preventDefault();
-      settings.get('fields').add(this.model.clone());
-      this.$el.modal('hide');
-    },
-  });
+      submit: function(e) {
+        e.preventDefault();
+        settings.get('fields').add(this.model.clone());
+        this.$el.modal('hide');
+      },
+    });
   
-  var EditFieldView = Backbone.View.extend({
-    el: $('#editFieldModal'),
+    var EditFieldView = Backbone.View.extend({
+      el: $('#editFieldModal'),
 
-    events: {
-      'click #editFieldCancel'  : 'onCancel',
-      'click #editFieldSubmit'  : 'submit',
-      'submit #editFieldForm'   : 'submit',
-      'shown'                   : 'shown',
-      'hidden'                  : 'onHidden',
-    },
+      events: {
+        'click #editFieldCancel'  : 'onCancel',
+        'click #editFieldSubmit'  : 'submit',
+        'submit #editFieldForm'   : 'submit',
+        'shown'                   : 'shown',
+        'hidden'                  : 'onHidden',
+      },
     
-    initialize: function() {
-      this._modelBinder = new Backbone.ModelBinder();
+      initialize: function() {
+        this._modelBinder = new Backbone.ModelBinder();
 
-      this.render();
+        this.render();
       
-      this.model.on('change', this.onChanged, this);
+        this.model.on('change', this.onChanged, this);
       
-      this.$el.modal('show');
-      this.processCancel = true;
-    },
+        this.$el.modal('show');
+        this.processCancel = true;
+      },
     
-    render: function() {
-      this._modelBinder.bind(this.model, this.el);
-    },
+      render: function() {
+        this._modelBinder.bind(this.model, this.el);
+      },
     
-    shown: function() {
-      this.$('input').first().focus();
-    },
+      shown: function() {
+        this.$('input').first().focus();
+      },
     
-    onHidden: function() {
-      this._modelBinder.unbind();
-    },
+      onHidden: function() {
+        this._modelBinder.unbind();
+      },
     
-    onChanged: function() {
-      this.revert || (this.revert = {});
+      onChanged: function() {
+        this.revert || (this.revert = {});
       
-      for (var field in this.model.changedAttributes()) {
-        if (this.revert.hasOwnProperty(field)) continue;
+        for (var field in this.model.changedAttributes()) {
+          if (this.revert.hasOwnProperty(field)) continue;
         
-        this.revert[field] = this.model.previous(field);
-      }
-    },
+          this.revert[field] = this.model.previous(field);
+        }
+      },
     
-    onCancel: function(e) {
-      this.cancel();
-      this.$el.modal('hide');
-    },
+      onCancel: function(e) {
+        this.cancel();
+        this.$el.modal('hide');
+      },
     
-    cancel: function() {
-      if (this.revert) {
-        this.model.off(null, this.onChanged, this);
-        this.model.set(this.revert);
-      }
+      cancel: function() {
+        if (this.revert) {
+          this.model.off(null, this.onChanged, this);
+          this.model.set(this.revert);
+        }
       
-      this.processCancel = false;
-    },
+        this.processCancel = false;
+      },
         
-    submit: function(e) {
-      e.preventDefault();
-      this.$el.modal('hide');
-    },
-  });
-  
-  var FieldView = Backbone.View.extend({
-    tagName: 'tr',
-    
-    template: $('#fieldTemplate').text(),
-    
-    events: {
-      'click .icon-remove'  : 'delete',
-      'click .namefield'    : 'edit',
-      'mouseover'           : 'onMouseOver',
-      'mouseleave'          : 'onMouseLeave',
-    },
-    
-    initialize: function() {
-      this.model.bind('change', this.render, this);
-      this.model.bind('destroy', this.remove, this);
-    },
-    
-    render: function() {
-      this.$el.html(Mustache.render(this.template, this.model.toJSON()));
-      return this;
-    },
-    
-    delete: function(e) {
-      e.preventDefault();
-      this.model.destroy();
-    },
-    
-    edit: function(e) {
-      e.preventDefault();
-      var editView = new EditFieldView({
-        model: this.model
-      });
-    },
-    
-    onMouseOver: function(e) {
-      this.$('.icon-remove')
-        .removeClass('invisible');
-    },
-    
-    onMouseLeave: function(e) {
-      this.$('.icon-remove')
-        .addClass('invisible');
-    }
-  });
-  
-  var SettingsView = Backbone.View.extend({
-    el: $('#admin-view'),
-    
-    events: {
-      'click #addField'   : 'addField',
-    },
-    
-    initialize: function() {
-      this.table = this.$('tbody');
-      
-      this._modelBinder = new Backbone.ModelBinder();
-      
-      this.newFieldView = new NewFieldView({
-        model: new Field
-      });
-      
-      this.model.get('fields').bind('add', this.onNewField, this);
-      
-      this.render();
-    },
-    
-    render: function() {
-      var self = this;
-      
-      this._modelBinder.bind(this.model, this.$('#details'));
-      this.model.get('fields').each($.proxy(this.onNewField, this));
-    },
-    
-    onNewField: function(field) {
-      var fieldView = new FieldView({
-        model: field
-      });
-      
-      this.table.append(fieldView.render().el);
-    },
-    
-    addField: function(e) {
-      this.newFieldView.show();
-    },
-  })
-  
-  var settings = new Settings({
-    name: 'passwords',
-    title: 'Passwords',
-    description: 'List of passwords',
-    
-    fields: [
-      {
-        name: 'name',
-        heading: 'Name',
-        placeholder: 'Account Name',
-        type: 'Name',
-        formType: 'text',
-        required: true,
-        default: '',
+      submit: function(e) {
+        e.preventDefault();
+        this.$el.modal('hide');
       },
-      {
-        name: 'url',
-        heading: 'Website',
-        placeholder: 'http://www.someplace.com',
-        type: 'Url',
-        formType: 'url',
-        required: true,
-        default: '',
+    });
+  
+    var FieldView = Backbone.View.extend({
+      tagName: 'tr',
+    
+      template: $('#fieldTemplate').text(),
+    
+      events: {
+        'click .icon-remove'  : 'delete',
+        'click .namefield'    : 'edit',
+        'mouseover'           : 'onMouseOver',
+        'mouseleave'          : 'onMouseLeave',
       },
-      {
-        name: 'username',
-        heading: 'User Name',
-        placeholder: 'john@doe.com',
-        type: 'String',
-        formType: 'text',
-        required: false,
+    
+      initialize: function() {
+        this.model.bind('change', this.render, this);
+        this.model.bind('destroy', this.remove, this);
       },
-      {
-        name: 'password',
-        heading: 'Password',
-        placeholder: 'password',
-        type: 'String',
-        formType: 'text',
-        required: false,
+    
+      render: function() {
+        this.$el.html(Mustache.render(this.template, this.model.toJSON()));
+        return this;
+      },
+    
+      delete: function(e) {
+        e.preventDefault();
+        this.model.destroy();
+      },
+    
+      edit: function(e) {
+        e.preventDefault();
+        var editView = new EditFieldView({
+          model: this.model
+        });
+      },
+    
+      onMouseOver: function(e) {
+        this.$('.icon-remove')
+          .removeClass('invisible');
+      },
+    
+      onMouseLeave: function(e) {
+        this.$('.icon-remove')
+          .addClass('invisible');
       }
-    ]
-  });
+    });
   
-  var settingsView = new SettingsView({
-    model: settings
-  });
+    var SettingsView = Backbone.View.extend({
+      el: $('#admin-view'),
+    
+      events: {
+        'click #addField'   : 'addField',
+      },
+    
+      initialize: function() {
+        this.table = this.$('tbody');
+      
+        this._modelBinder = new Backbone.ModelBinder();
+      
+        this.newFieldView = new NewFieldView({
+          model: new Field
+        });
+      
+        this.model.get('fields').bind('add', this.onNewField, this);
+      
+        this.render();
+      },
+    
+      render: function() {
+        var self = this;
+      
+        this._modelBinder.bind(this.model, this.$('#details'));
+        this.model.get('fields').each($.proxy(this.onNewField, this));
+      },
+    
+      onNewField: function(field) {
+        var fieldView = new FieldView({
+          model: field
+        });
+      
+        this.table.append(fieldView.render().el);
+      },
+    
+      addField: function(e) {
+        this.newFieldView.show();
+      },
+    })
+    
+    /**
+     * Module exports
+     */
+    exports.Settings = Settings;
+    exports.SettingsView = SettingsView;
+    
+  })(window.Admin);
 })
